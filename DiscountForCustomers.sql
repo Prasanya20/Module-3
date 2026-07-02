0f5304c61225f40b0ff93672ed0ba2675d7028ea
Prasanya20 <prasanya.medapati2006@gmail.com>
2026-07-02 09:37:00 +0530

    Create DiscountForCustomers.sql

---
+++ DiscountForCustomers.sql
@@ -0,0 +1,21 @@
+DECLARE
+    CURSOR cust_cursor IS
+        SELECT c.CustomerID, c.Age, l.LoanID, l.InterestRate
+        FROM Customers c
+        JOIN Loans l
+        ON c.CustomerID = l.CustomerID;
+
+BEGIN
+    FOR rec IN cust_cursor LOOP
+        IF rec.Age > 60 THEN
+            UPDATE Loans
+            SET InterestRate = InterestRate - 1
+            WHERE LoanID = rec.LoanID;
+
+            DBMS_OUTPUT.PUT_LINE('1% discount applied to Loan ID: ' || rec.LoanID);
+        END IF;
+    END LOOP;
+
+    COMMIT;
+END;
+/
